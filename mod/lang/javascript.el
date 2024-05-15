e8eb70c3a714f7888b53e4167ba5bc4dccb99978
DeciPaliz <unat7919@gmail.com>
2024-05-15 19:00:50 +0300

    remove unused

--- mod/lang/javascript.el
+++ mod/lang/javascript.el
@@ -3,11 +3,6 @@
 (package! jsdoc)
 (package! typescript-mode)
 
-(defun rc/js-keymap ()
-  (tyrant-def
-    "c" (cons "javascript" (make-sparse-keymap))
-    "cd" 'jsdoc))
-
 (rlhooks! (typescript-mode-hook js-mode-hook)
           (#'lsp #'hs-minor-mode (lambda ()
                                    (tyrant-def
