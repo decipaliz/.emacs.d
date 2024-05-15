db59761260104c82a5bd0eec293a43d2870f96bf
DeciPaliz <unat7919@gmail.com>
2024-05-15 19:34:25 +0300

    fix

--- mod/lang/java.el
+++ mod/lang/java.el
@@ -1,6 +1,6 @@
 ;; -*- lexical-binding: t -*-
 
-(hooks java-mode-hook #'hs-minor-mode)
+(hooks! java-mode-hook #'hs-minor-mode)
 (when! lsp
        (package! lsp-java)
-       (hooks java-mode-hook #'lsp))
+       (hooks! java-mode-hook #'lsp))
