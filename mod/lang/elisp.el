ee57bb2b3831fb12a3d5c16c73230563cef30ec2
DeciPaliz <unat7919@gmail.com>
2024-05-15 20:24:52 +0300

    disable flycheck for elisp

--- mod/lang/elisp.el
+++ mod/lang/elisp.el
@@ -1,4 +1,4 @@
 ;; -*- lexical-binding: t -*-
 
 (when! flycheck
-       (setq flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
+       (setq flycheck-disabled-checkers '(emacs-lisp-checkdoc emacs-lisp)))
