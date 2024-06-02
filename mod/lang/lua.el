809c5ae00044363024e5226536549160fba27133
DeciPaliz <unat7919@gmail.com>
2024-06-02 21:27:24 +0300

    lua offset

--- mod/lang/lua.el
+++ mod/lang/lua.el
@@ -1,7 +1,8 @@
 ;; -*- lexical-binding: t -*-
 
 (package! lua-mode
-          (hooks! lua-mode-hook #'hs-minor-mode))
+          (hooks! lua-mode-hook #'hs-minor-mode)
+          (setq lua-indent-level 4))
 
 (when! lsp
        (hooks! lua-mode-hook #'lsp))
